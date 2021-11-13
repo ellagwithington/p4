974df56b59c7b60f620ccd2209046066a6018e0d
ellagwithington <85177653+ellagwithington@users.noreply.github.com>
2021-11-12 21:59:56 -0500

    Add files via upload

---
+++ InventoryReader.java
@@ -0,0 +1,44 @@
+//Program by Ella Withington 
+import java.util.HashMap;
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.*;
+
+public class InventoryReader {
+    static HashMap<String, String> data = new HashMap<String, String>();
+public static void main(String[] args) {
+readFile("inventory.xml");
+}
+public static void readFile(String filename){
+    File dataFile = new File(filename);
+    if ( ! dataFile.exists() ) {
+        System.out.println("No data file found.");
+        System.exit(1);
+    }
+    try( Scanner scanner = new Scanner(dataFile) ) {
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            int separatorPosition = input.indexOf('<');
+            int separatorPosition2 = input.indexOf('>');
+            input = input.substring(separatorPosition + 1, separatorPosition2);
+            if(input.equals("stockNumber")){
+                String value = scanner.nextLine();
+                data.put("stockNumber", value);
+            }
+            if(input.equals("description")){
+                String value = scanner.nextLine();
+                data.put("description", value);
+            }
+        }
+    }
+    catch (IOException e) {
+        System.out.println("Error in data file.");
+        System.exit(1);
+    }
+    for (String key : data.keySet()) {
+        System.out.print("Key = " + key + ", ");
+        System.out.println("Data = " + data.get(key));
+        }
+}
+}
